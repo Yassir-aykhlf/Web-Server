2b7bd04cf6d14a4d47c9c91f7d1eeee725b9a62e
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-13 15:55:53 +0000

    Feat: main function with basic argument handling

---
+++ srs/main.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::string configFile;
+    Server      server; // TODO: create server class
+
+    if (argc > 2) {
+        std::cerr << "Usage: " << argv[0] << " [config_file]" << std::endl;
+        return 1;
+    }
+    if (argc == 2) {
+        configFile = argv[1];
+    } else {
+        configFile = "config/default.conf";
+    }
+
+    if (!server.init(configFile)) {
+        // log error
+        return 1;
+    }
+    server.run();
+    // log server shut down
+    return 0;
+}
