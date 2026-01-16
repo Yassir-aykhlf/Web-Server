bb8c6e996d0fe3393925387b2df71d669b1dd5a8
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-16 16:22:59 +0000

    feat: header file with placeholder class declarations

---
+++ include/webserv.hpp
@@ -0,0 +1,16 @@
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
+
+class   Server;
+class   Client;
+class   Config;
+class   ServerConfig;
+class   LocationConfig;
+class   EvenLoop;
+class   HttpRequest;
+class   HttpResponse;
+class   HttpParser;
+class   RequestHandler;
+class   CgiHandler;
+
+#endif // WEBSERV_HPP
